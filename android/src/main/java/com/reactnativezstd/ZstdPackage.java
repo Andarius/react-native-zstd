a25ed44410f0ab28e26d76951ff1cbc3ef21a543
Julien <julien@tracthor-mini.local>
2024-04-02 09:48:34 +0200

    chore: fix lint

--- android/src/main/java/com/reactnativezstd/ZstdPackage.java
+++ android/src/main/java/com/reactnativezstd/ZstdPackage.java
@@ -43,34 +43,3 @@ public class ZstdPackage extends TurboReactPackage {
     };
   }
 }
-
-/**
- * old
- * package com.reactnativezstd;
-
-import androidx.annotation.NonNull;
-
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-
-import java.util.Collections;
-import java.util.List;
-
-public class ZstdPackage implements ReactPackage {
-  @NonNull
-  @Override
-  public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-    return Collections.singletonList(new ZstdModule(reactContext));
-  }
-
-  @NonNull
-  @Override
-  public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-    return Collections.emptyList();
-  }
-}
-
-
- */
--- ios/Zstd.h
+++ ios/Zstd.h
@@ -1,11 +1,15 @@
-#import <React/RCTBridgeModule.h>
-
 #ifdef __cplusplus
-
 #import "react-native-zstd.h"
-
 #endif
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNZstdSpec.h"
+
+@interface Zstd : NSObject <NativeZstdSpec>
+#else
+#import <React/RCTBridgeModule.h>
+
 @interface Zstd : NSObject <RCTBridgeModule>
+#endif
 
 @end
