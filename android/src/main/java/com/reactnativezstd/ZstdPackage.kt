0273d9ecca1ecfd00cf057b0e27a400575e54c9e
julien <julien.brayere@obitrain.com>
2026-02-26 21:31:18 +0100

    chore: cleaning

---
+++ android/src/main/java/com/reactnativezstd/ZstdPackage.kt
@@ -0,0 +1,22 @@
+package com.reactnativezstd
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfoProvider
+import com.margelo.nitro.zstd.reactnativezstdOnLoad
+
+class ZstdPackage : BaseReactPackage() {
+    override fun getModule(
+        name: String,
+        reactContext: ReactApplicationContext,
+    ): NativeModule? = null
+
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider = ReactModuleInfoProvider { HashMap() }
+
+    companion object {
+        init {
+            reactnativezstdOnLoad.initializeNative()
+        }
+    }
+}
