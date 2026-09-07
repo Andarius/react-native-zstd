aee89ec1f0eb4c61990059ea4bc89eb4f004aa8c
Julien Brayere <julien.brayere@obitrain.com>
2026-09-07 11:43:22 +0200

    feat: migrate Zstd example to Expo development builds

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,18 +1,9 @@
 const path = require('path');
-const { getDefaultConfig } = require('@react-native/metro-config');
-const { getConfig } = require('react-native-builder-bob/metro-config');
-const pkg = require('../package.json');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withMetroConfig } = require('react-native-monorepo-config');
 
-const root = path.resolve(__dirname, '..');
-
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
+module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+  root: path.resolve(__dirname, '..'),
+  dirname: __dirname,
+  conditions: ['source'],
 });
